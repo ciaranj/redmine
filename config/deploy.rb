2354be5713ea53b70be26c0646289cf201a7d6b3
Dan Hodos <danhodos@gmail.com>
2008-12-16 07:38:15 -0600

    B-01091 - adds proper restart task for deploying with passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,13 +17,14 @@ role :app, "scrum01.managed.contegix.com"
 role :web, "scrum01.managed.contegix.com"
 role :db, "scrum01.managed.contegix.com", :primary => true
 
-after "deploy:update_code", :symlink_in_dblogin_yml_file
-task :symlink_in_dblogin_yml_file do
+after "deploy:update_code", :redmine_symlinks
+task :redmine_symlinks do
   run "ln -fns  #{shared_path}/dblogin.yml #{release_path}/config/dblogin.yml"
 end
 
 namespace :deploy do
-  task :restart do
-    ''
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
   end
 end
