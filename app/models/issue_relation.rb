db494bc5708020b4259972caa711b009396bfa74
Dan Hodos <danhodos@gmail.com>
2008-12-23 19:32:45 -0600

    Re: #1 - puts the new type of issue relation back into the model directly (because of the validation on relation_type that will be based on the old (wrong) set

--- app/models/issue_relation.rb
+++ app/models/issue_relation.rb
@@ -23,11 +23,13 @@ class IssueRelation < ActiveRecord::Base
   TYPE_DUPLICATES   = "duplicates"
   TYPE_BLOCKS       = "blocks"
   TYPE_PRECEDES     = "precedes"
+  TYPE_COMPOSES     = 'composes'
   
   TYPES = { TYPE_RELATES =>     { :name => :label_relates_to, :sym_name => :label_relates_to, :order => 1 },
             TYPE_DUPLICATES =>  { :name => :label_duplicates, :sym_name => :label_duplicated_by, :order => 2 },
             TYPE_BLOCKS =>      { :name => :label_blocks, :sym_name => :label_blocked_by, :order => 3 },
-            TYPE_PRECEDES =>    { :name => :label_precedes, :sym_name => :label_follows, :order => 4 },
+            TYPE_PRECEDES =>    { :name => :label_precedes, :sym_name => :label_follows, :order => 4 }, 
+            TYPE_COMPOSES =>    { :name => :label_includes, :sym_name => :label_belongs_to, :order => 5 }
           }.freeze
   
   validates_presence_of :issue_from, :issue_to, :relation_type
--- vendor/plugins/redmine_task_board/lib/scrum_alliance/redmine/issue_relation_extensions.rb
+++ vendor/plugins/redmine_task_board/lib/scrum_alliance/redmine/issue_relation_extensions.rb
@@ -2,15 +2,15 @@ module ScrumAlliance
   module Redmine
     module IssueRelationExtensions
       def self.included(klass)
-        klass.const_set("TYPE_COMPOSES", 'composes')
-
-        types = klass::TYPES.dup
-        types[klass::TYPE_COMPOSES] = { :name => :label_includes, :sym_name => :label_belongs_to, :order => 5 }
-
-        klass.send :remove_const, "TYPES"
-        klass.const_set("TYPES", types)
-        
-        klass::TYPES.freeze
+        # klass.const_set("TYPE_COMPOSES", 'composes')
+        # 
+        # types = klass::TYPES.dup
+        # types[klass::TYPE_COMPOSES] = { :name => :label_includes, :sym_name => :label_belongs_to, :order => 5 }
+        # 
+        # klass.send :remove_const, "TYPES"
+        # klass.const_set("TYPES", types)
+        # 
+        # klass::TYPES.freeze
       end
     end # IssueRelationExtensions
   end # Redmine
