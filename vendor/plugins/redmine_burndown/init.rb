234690b9c6aad1fd2747b60b69144d820438b586
Dan Hodos <danhodos@gmail.com>
2009-01-25 09:21:17 -0600

    Makes the burndown plugin completely independent of other SA plugins in preparation for release

--- vendor/plugins/redmine_burndown/init.rb
+++ vendor/plugins/redmine_burndown/init.rb
@@ -2,12 +2,18 @@ require 'redmine'
 require 'gchart' 
 
 require_dependency 'burndown_listener'
+require_dependency 'scrum_alliance/redmine/current_version_extension'
+
+require 'dispatcher'
+Dispatcher.to_prepare do
+  Project.class_eval { include ScrumAlliance::Redmine::CurrentVersionExtension }
+end
 
 Redmine::Plugin.register :burndown do
   name 'Redmine Burndown plugin'
   author 'Dan Hodos'
   description 'Generates a simple Burndown chart for using Redmine in Scrum environments'
-  version '1.1.0'
+  version '1.1.1'
 
   project_module :burndowns do  
     permission :show_burndown, :burndowns => :show, :public => true
