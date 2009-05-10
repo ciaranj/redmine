2390f4af57d262cf90c9790a5e9a054fdfb6915c
ciaranj <ciaranj@gmail.com>
2009-05-10 14:23:59 +0100

    Put a little bit of nil-safety back into the css adding code's life.

--- vendor/plugins/redmine_backlogs/lib/scrum_alliance/redmine/add_extra_classes_to_issue_extension.rb
+++ vendor/plugins/redmine_backlogs/lib/scrum_alliance/redmine/add_extra_classes_to_issue_extension.rb
@@ -7,7 +7,11 @@ module ScrumAlliance
         end
       end
       def css_classes_with_tracker_name
-        "#{css_classes_without_tracker_name} type-#{@tracker.name.downcase}"
+        if @tracker.nil?
+          css_classes_without_tracker_name
+        else
+          "#{css_classes_without_tracker_name} type-#{@tracker.name.downcase}"
+        end
       end    
     end # AddExtraClassesToIssueExtension
   end # Redmine
